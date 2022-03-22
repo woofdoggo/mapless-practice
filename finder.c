592b03308b9668d797224bf5c374e9c332018df0
woofdoggo <woofwoofdoggo@protonmail.com>
2022-03-22 19:24:49 -0400

    Improve random for finder
    Use a simple xorshift implementation to fix rand being allegedly horrible when crosscompiling for Windows. As a bonus, generates 64 bit seeds instead of 32 bit. Should be better overall

--- finder.c
+++ finder.c
@@ -41,11 +41,31 @@ uint64_t gen_mapless(uint64_t start) {
     }
 }
 
+// on linux with glibc, rand actually did a somewhat acceptable job
+// (~700 collisions out of ~150k generated seeds)
+//
+// when crosscompiling for windows, it is allegedly a lot worse: >95%
+// collision rate. thanks to vondraker for reporting this issue o/
+//
+// this is just a simple xorshift implementation to try and avoid the issue.
+// it also generates 64 bit seeds instead of 32 bit, so it should (hopefully)
+// be better regardless.
+
+uint64_t rand_x;
+uint64_t rand_iter() {
+    rand_x ^= (rand_x << 7);
+    rand_x ^= (rand_x >> 2);
+    rand_x ^= (rand_x << 3);
+    return rand_x;
+}
+
 int main() {
     srand(time(NULL));
+    rand_x = rand();
+    rand_iter();
 
     while (1) {
-        uint64_t seed = gen_mapless(rand());
+        uint64_t seed = gen_mapless(rand_iter());
         printf("%" PRId64 "\n", seed);
     }
 }
